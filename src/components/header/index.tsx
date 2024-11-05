d09a33aed21dd11212b593e86a86637c796c888c
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-11-05 13:42:01 -0300

    Criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,37 @@
-import styles from './styles.module.scss';
+"use client";
+
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
 
 export function Header() {
+  const [top, setTop] = useState(true);
+
+  const scrollHandler = () => {
+    window.scrollY > 10 ? setTop(false) : setTop(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
+
   return (
-    <header>
-      <h2>Header</h2>
+    <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.contentLogo}>
+            <Link href="/">Dev Motors</Link>
+          </div>
+
+          <nav className={styles.nav}>
+            <Link href="/">HOME</Link>
+            <Link href="/#servicos">SERVIÇOS</Link>
+            <Link href="/#contatos">CONTATOS</Link>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
