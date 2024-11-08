d72403ff8e6c61ac99baf1721b578846a9342f9b
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-11-08 16:51:58 -0300

    Estilzando submenu

--- src/components/home/submenu/index.tsx
+++ src/components/home/submenu/index.tsx
@@ -1,20 +1,57 @@
-import Link from "next/link";
-import styles from "./styles.module.scss";
-import { Menu } from "lucide-react";
+"use client"
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import styles from './styles.module.scss'
 
-export function Submenu() {
-  return (
+import { X, Menu} from "lucide-react"
+
+export function Submenu(){
+  const [isOpen, setIsOpen] = useState(false);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      if(window.innerWidth > 768){
+        setIsOpen(false);
+      }
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    return () => window.removeEventListener("resize", handleResize)
+
+  }, [])
+
+  function toggleMenu(){
+    setIsOpen(!isOpen);
+  }
+
+  return(
     <section className={styles.submenu}>
-      <div className={styles.submenuIcon}>
+      <div className={styles.submenuIcon} onClick={toggleMenu}>
         <Menu size={34} color="#121212" />
-        Menu
+        Serviços
       </div>
-      <ul className={styles.ul}>
+
+      <ul className={`${styles.ul} ${isOpen ? styles.open : ""}`}>
+
+        {isOpen && (
+          <button onClick={toggleMenu} className={styles.closeButton}>
+            <X size={54} color="#121212" />
+          </button>
+        )}
+
+
         <li>
-          <Link href="/post/pagina-1">Página 1</Link>
-          <Link href="/post/pagina-2">Página 2</Link>
+          <Link href="/post/pagina-1">
+            Pagina 1
+          </Link>
+        </li>
+        <li>
+          <Link href="/post/pagina-2">
+            Pagina 2
+          </Link>
         </li>
       </ul>
     </section>
-  );
+  )
 }
