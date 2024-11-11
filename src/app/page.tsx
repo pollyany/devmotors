2d5f8d6968f2682f926b30b5d72e240b7758a7f6
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-11-11 09:12:46 -0300

    Consumindo cms no projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Submenu } from "@/components/home/submenu";
+import { getDataHome } from "@/utils/actions/get-data";
+import { HomeProps } from "@/utils/home.type";
 
-export default function Home() {
+export default async function Home() {
+  const { object }: HomeProps = await getDataHome();
+
   return (
     <main>
       <Submenu/>
