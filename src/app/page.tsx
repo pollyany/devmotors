a2cc8a269aaa8fee055c2c7faeba9b7e12baa4c5
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-01-12 10:55:28 -0300

    Componente Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Submenu } from "@/components/home/submenu";
-import { getDataHome } from "@/utils/actions/get-data";
+import { getDataHome } from '@/utils/actions/get-data'
 import { HomeProps } from "@/utils/home.type";
+import { Hero } from '@/components/hero'
+import { Phone } from 'lucide-react'
 
 export default async function Home() {
   const { object }: HomeProps = await getDataHome();
@@ -8,6 +10,14 @@ export default async function Home() {
   return (
     <main>
       <Submenu/>
+
+      <Hero
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        icon={<Phone size={24} color="#FFF" />}
+      />
     </main>
   );
 }
