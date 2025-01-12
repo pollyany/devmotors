d1b18b639fe6c893164ffdde65f03fc0f5510582
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-01-12 18:13:39 -0300

    Criando página dinâmica

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss'
+import { Hero } from '@/components/hero';
 import { getItemBySlug } from '@/utils/actions/get-data'
-import { PostProps } from '@/utils/post.type';
+import { PostProps } from '@/utils/post.type'
+import { Phone } from 'lucide-react';
+import { Container } from '@/components/container';
+import Image from 'next/image';
 
 export default async function Page({ params: { slug } }: {
   params: { slug: string }
@@ -8,8 +12,49 @@ export default async function Page({ params: { slug } }: {
   const { objects }: PostProps = await getItemBySlug(slug);
 
   return(
-    <div>
-      <h1>PAGINA {slug}</h1>
-    </div>
+    <>
+      <Hero
+        heading={objects[0].title}
+        buttonTitle={objects[0].metadata.button.title}
+        buttonUrl={objects[0].metadata.button.url}
+        bannerUrl={objects[0].metadata.banner.url}
+        icon={<Phone size={24} color="#FFF" />}
+      />
+
+      <Container>
+        <section className={styles.about}>
+
+          <article className={styles.innerAbout}>
+            <h1 className={styles.title}>
+              {objects[0].metadata.description.title}
+            </h1>
+            <p>
+              {objects[0].metadata.description.text}
+            </p>
+
+            {objects[0].metadata.description.button_active && (
+              <a
+                href={objects[0].metadata.description.button_url as string}
+                target='_blank'
+                className={styles.link}
+              >
+                {objects[0].metadata.description.button_title}
+              </a>
+            )}
+          </article>
+
+          <div className={styles.bannerAbout}>
+            <Image
+              className={styles.imageAbout}
+              alt={objects[0].title}
+              quality={100}
+              fill={true}
+              src={objects[0].metadata.description.banner.url}
+            />
+          </div>
+
+        </section>
+      </Container>
+    </>
   )
 }
